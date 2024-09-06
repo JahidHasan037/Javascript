b48e7c7a3e59ab65559ffe5fa80ed88b76468487
JahidHasan037 <jahid20cse037@gmail.com>
2024-09-06 19:18:00 +0800

    sumOfArray Element

---
+++ SumOfArray.js
@@ -0,0 +1,12 @@
+function sum(numbers)
+{
+    let sum=0;
+    for(let number of numbers)
+    {
+        sum+=number;
+    }
+    return sum;
+}
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+let result=sum(numbers);
+console.log(result);
