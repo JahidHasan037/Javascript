f54b9587736ce1552e958da5a22d714b13349b95
JahidHasan037 <jahid20cse037@gmail.com>
2024-09-06 19:45:21 +0800

    Avarage ofm array

---
+++ AvarageOfArray.js
@@ -0,0 +1,14 @@
+function AvarageOfArray(number)
+{
+    let sum=0,cnt=0;
+  for(let number of numbers)
+  {
+    sum+=number;
+    cnt++;
+  }
+  let avg=sum/cnt;
+  return avg;
+}
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+let result=AvarageOfArray(numbers);
+console.log(result);
