81bceffd8b543b8b88c028709bf38d49807618d5
JahidHasan037 <jahid20cse037@gmail.com>
2024-09-07 13:29:31 +0800

    Swap

---
+++ Swap.js
@@ -0,0 +1,14 @@
+// let a=5;
+// let b=6;
+// console.log(a,b);
+// [a,b]=[b,a];
+// console.log(a,b);
+
+
+///Swap using temp variable
+let a=3;
+let b=6;
+let temp=a;
+a=b;
+b=temp;
+console.log(a,b);
