9a9a16285c674040d6e68360e4f8515ca4087b17
JahidHasan037 <jahid20cse037@gmail.com>
2024-09-07 12:56:44 +0800

    Math Related problem

---
+++ Math.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5,67,8,9];
+let result=Math.min(1,2,3,4,56);
+console.log(result);
+let x=4.5;
+console.log(Math.ceil(x));
+console.log(Math.floor(x));
+console.log(Math.round(x));
+console.log(Math.abs(5-10));
